c439f9ee210402500a1cc44f94fa485ba549be22
niranjannlc <77341018@nou.edu.np>
2023-08-26 13:39:17 +0545

    search array

--- src/main/kotlin/day2/searchSort/SearchArray.kt
+++ src/main/kotlin/day2/searchSort/SearchArray.kt
@@ -2,10 +2,9 @@ package day2.searchSort
 
 /*
 A step array is an array of integers where each element has a difference of at most k with
-its neighbor. Given a key x, we need to find the index value of x if multiple-element exist to return the first occurrence of the key.
+its neighbor. Given a key x, we need to find the index value of x
+if multiple-element exist to return the first occurrence of the key.
 Examples:
-
-
 Input : arr[] = {4, 5, 6, 7, 6}
            k = 1
            x = 6
@@ -20,6 +19,11 @@ The index of 60 is 5
 
  */
 fun findFirstOccurrence(arr: IntArray, k: Int, x: Int): Int {
-
+    var i =0
+   while(i < arr.size )
+    {
+        if (arr[i]==x) return i
+        i = i + Math.max(1, Math.abs(arr[i]))
+    }
     return -1
 }

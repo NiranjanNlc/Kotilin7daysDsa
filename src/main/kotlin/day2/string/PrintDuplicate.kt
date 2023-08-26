737d2cf01819837f82015a18b8e0d7b23ec972cf
niranjannlc <77341018@nou.edu.np>
2023-08-26 11:55:58 +0545

    pront duplicate

--- src/main/kotlin/day2/string/PrintDuplicate.kt
+++ src/main/kotlin/day2/string/PrintDuplicate.kt
@@ -12,6 +12,13 @@ e, count = 4
 g, count = 2
 k, count = 2
 s, count = 2*/
-fun findDuplicateCharacters(input: String): Map<Char, Int> {
-    return mutableMapOf()
+fun findDuplicateCharacters(input: String): String {
+    var count = mutableMapOf<Char, Int>()
+    for (ch in input) {
+        count[ch] = count.getOrDefault(ch, 0) + 1
+    }
+    println(" $count for $input" )
+    return count.entries.sortedBy { it.key }.joinToString(", ") {
+        "[${it.key}, count = ${it.value}]"
+    }
 }

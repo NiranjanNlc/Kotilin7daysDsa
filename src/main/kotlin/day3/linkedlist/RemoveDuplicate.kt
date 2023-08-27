29966595f22799501e20e1b18a9b3bac4a840426
niranjannlc <77341018@nou.edu.np>
2023-08-27 12:45:55 +0545

    ➖ remove duplicate from linked list

--- src/main/kotlin/day3/linkedlist/RemoveDuplicate.kt
+++ src/main/kotlin/day3/linkedlist/RemoveDuplicate.kt
@@ -1,4 +1,21 @@
 package day3.linkedlist
 
-class RemoveDuplicate {
+import day3.LinkedListNode
+
+fun removeDuplicates(head: LinkedListNode?): LinkedListNode? {
+    // Implementation of removeDuplicates function
+    var curent = head
+    var value = mutableListOf<Int>()
+    while (curent!= null)
+    {
+        var next = curent.nextNode
+        if (value.contains(next?.value))
+        {
+            curent.nextNode = next?.nextNode
+        }else{
+            curent = next
+        }
+
+    }
+    return head
 }
--- src/test/kotlin/day3/linkedlist/RemoveDuplicateTest.kt
+++ src/test/kotlin/day3/linkedlist/RemoveDuplicateTest.kt
@@ -1,5 +1,57 @@
 package day3.linkedlist
 
-import org.junit.Assert.*
+import day3.createLinkedList
+import day3.linkedlist.util.assertLinkedListEquals
+import org.junit.Test
+import org.junit.Assert.assertEquals
 
-class RemoveDuplicateTest
+
+class RemoveDuplicatesTest {
+
+    @Test
+    fun testRemoveDuplicatesWithDuplicates() {
+        val head = createLinkedList(1, 2, 2, 3, 4, 4, 5)
+        val expected = createLinkedList(1, 2, 3, 4, 5)
+
+        val result = removeDuplicates(head)
+        assertLinkedListEquals(expected, result)
+    }
+
+    @Test
+    fun testRemoveDuplicatesWithNoDuplicates() {
+        val head = createLinkedList(5, 10, 15, 20, 25)
+        val expected = createLinkedList(5, 10, 15, 20, 25)
+
+        val result = removeDuplicates(head)
+        assertLinkedListEquals(expected, result)
+    }
+
+    @Test
+    fun testRemoveDuplicatesWithDuplicatesAtStart() {
+        val head = createLinkedList(5, 5, 10, 15, 20)
+        val expected = createLinkedList(5, 10, 15, 20)
+
+        val result = removeDuplicates(head)
+        assertLinkedListEquals(expected, result)
+    }
+
+    @Test
+    fun testRemoveDuplicatesWithDuplicatesAtEnd() {
+        val head = createLinkedList(5, 10, 15, 20, 20)
+        val expected = createLinkedList(5, 10, 15, 20)
+
+        val result = removeDuplicates(head)
+        assertLinkedListEquals(expected, result)
+    }
+
+    @Test
+    fun testRemoveDuplicatesWithEmptyList() {
+        val head = null
+        val expected = null
+
+        val result = removeDuplicates(head)
+        assertLinkedListEquals(expected, result)
+    }
+
+
+}

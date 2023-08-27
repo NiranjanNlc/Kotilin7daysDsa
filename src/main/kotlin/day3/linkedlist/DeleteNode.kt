21e81b1c62c171c63bef46d7be0154edaeed02bc
niranjannlc <77341018@nou.edu.np>
2023-08-27 12:29:18 +0545

    🗑️ delete from the linked list 🔗

--- src/main/kotlin/day3/linkedlist/DeleteNode.kt
+++ src/main/kotlin/day3/linkedlist/DeleteNode.kt
@@ -1,4 +1,23 @@
 package day3.linkedlist
 
-class DeleteNode {
+import day3.LinkedListNode
+
+/*
+* You are given a singly linked list and a target value.
+* Implement a function deleteNode to delete the first
+*  occurrence of the target value from the linked list.
+* If the target value does not exist in the list, no changes should be made.
+* Your task is to modify the linked list in-place and return the modified head.*/
+fun deleteNode(head: LinkedListNode?, target: Int): LinkedListNode? {
+    var currentNode = head
+    if (currentNode?.value == target)
+        return currentNode.nextNode
+    while (currentNode != null) {
+        var next = currentNode.nextNode
+        if (next?.value == target) {
+            currentNode.nextNode = next.nextNode
+        }
+        currentNode = next
+    }
+    return head
 }
--- src/test/kotlin/day3/linkedlist/util/Assertion.kt
+++ src/test/kotlin/day3/linkedlist/util/Assertion.kt
@@ -1,6 +1,7 @@
 package day3.linkedlist.util
 
 import day3.LinkedListNode
+import day3.displayLinkedList
 import org.junit.Assert.assertEquals
 
 
@@ -8,6 +9,8 @@ fun assertLinkedListEquals(expected: LinkedListNode?, actual: LinkedListNode?) {
     // assertLinkedListEquals implementation here
     var expectedCurrent = expected
     var actualCurrent = actual
+    displayLinkedList(expected)
+    displayLinkedList(actual)
     while (expectedCurrent != null && actualCurrent != null) {
         assertEquals(expectedCurrent.value, actualCurrent.value)
         expectedCurrent = expectedCurrent.nextNode

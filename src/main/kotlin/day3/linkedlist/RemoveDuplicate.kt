e0ca0bcfa7291d807a450be88b4e60d36a708d1e
niranjannlc <77341018@nou.edu.np>
2023-08-29 10:01:15 +0545

    remove dupliiacate

--- src/main/kotlin/day3/linkedlist/RemoveDuplicate.kt
+++ src/main/kotlin/day3/linkedlist/RemoveDuplicate.kt
@@ -6,6 +6,7 @@ fun removeDuplicates(head: LinkedListNode?): LinkedListNode? {
     // Implementation of removeDuplicates function
     var curent = head
     var value = mutableListOf<Int>()
+    curent?.let { value.add(it.value) }
     while (curent!= null)
     {
         var next = curent.nextNode
@@ -13,6 +14,7 @@ fun removeDuplicates(head: LinkedListNode?): LinkedListNode? {
         {
             curent.nextNode = next?.nextNode
         }else{
+            next?.let { value.add(it.value) }
             curent = next
         }
 
